1e7cb855f07cbc1acdbff70cc647fbbf22559bd9
Bill-icious <bill.lester@althanco.com>
2025-05-21 12:32:27 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-// your Three.js Earth logic with updated paths
-// snippet example:
-map: THREE.ImageUtils.loadTexture('earthmap1k.jpg'),
-bumpMap: THREE.ImageUtils.loadTexture('earthbump.jpg'),
-specularMap: THREE.ImageUtils.loadTexture('specularmap.jpg'),
-cloudMap: THREE.ImageUtils.loadTexture('earthCloud.png'),
-backgroundMap: THREE.ImageUtils.loadTexture('galaxy.png'),
